423aabe0456b359acebbb8bc6c0e9790d7314369
Samuel0102 <fsamuelpacheco@gmail.com>
2024-12-07 21:32:24 -0300

    feat(about): add about section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./core/components/header/header.component";
 import {MobileMenuComponent} from "./core/components/mobile-menu/mobile-menu.component";
 import {HomeComponent} from "./features/home/home/home.component";
+import {AboutComponent} from "./features/about/about/about.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MobileMenuComponent, HomeComponent],
+  imports: [RouterOutlet, HeaderComponent, MobileMenuComponent, HomeComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
