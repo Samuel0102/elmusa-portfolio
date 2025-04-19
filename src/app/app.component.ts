26d05725c550b7e7809a456766eeb35e261aa9d1
Samuel0102 <fsamuelpacheco@gmail.com>
2025-04-18 21:02:00 -0300

    feat(background): add moving particles in background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
 import {HeaderComponent} from "./core/components/header/header.component";
 import {MobileMenuComponent} from "./core/components/mobile-menu/mobile-menu.component";
 import {HomeComponent} from "./features/home/views/home/home.component";
@@ -9,13 +8,14 @@ import {SkillsComponent} from "./features/skills/views/skills/skills.component";
 import {EducationComponent} from "./features/education/views/education/education.component";
 import {ProjectsComponent} from "./features/projects/views/projects/projects.component";
 import {FooterComponent} from "./core/components/footer/footer.component";
-import {faArrowDown} from "@fortawesome/free-solid-svg-icons";
+import {IParticlesProps, NgParticlesService, NgxParticlesModule} from "@tsparticles/angular";
+import {Container, MoveDirection, OutMode} from "@tsparticles/engine";
+import { loadSlim } from "@tsparticles/slim";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet,
     HeaderComponent,
     MobileMenuComponent,
     HomeComponent,
@@ -24,12 +24,85 @@ import {faArrowDown} from "@fortawesome/free-solid-svg-icons";
     SkillsComponent,
     EducationComponent,
     ProjectsComponent,
-    FooterComponent
+    FooterComponent,
+    NgxParticlesModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'elmusa-portfolio';
-  protected readonly arrowDownIcon = faArrowDown;
+
+  public id = "tsparticles";
+  public particlesOptions: IParticlesProps = {
+    fullScreen: {
+      enable: true,
+      zIndex: -1
+    },
+    background: {
+      color: {
+        value: "#151019",
+      },
+    },
+    fpsLimit: 120,
+    interactivity: {
+      events: {
+        resize: {
+          enable: true,
+        },
+      },
+    },
+    particles: {
+      color: {
+        value: "#2596be",
+      },
+      links: {
+        color: "#2596be",
+        distance: 150,
+        enable: true,
+        opacity: 0.5,
+        width: 0.5,
+      },
+      move: {
+        direction: MoveDirection.none,
+        enable: true,
+        outModes: {
+          default: OutMode.bounce,
+        },
+        random: false,
+        speed: 1,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          height: 800,
+          width: 800,
+        },
+        value: 80,
+      },
+      opacity: {
+        value: 0.5,
+      },
+      shape: {
+        type: "circle",
+      },
+      size: {
+        value: { min: 1, max: 5 },
+      },
+    },
+    detectRetina: true,
+  };
+
+  constructor(private readonly ngParticlesService: NgParticlesService) {}
+
+  ngOnInit(): void {
+    this.ngParticlesService.init(async (engine) => {
+      await loadSlim(engine);
+    });
+  }
+
+  particlesLoaded(container: Container): void {
+    console.log(container);
+  }
 }
