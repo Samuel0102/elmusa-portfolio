f9db2acaa381519928396667b85876a8b0c52fd7
Samuel0102 <fsamuelpacheco@gmail.com>
2025-04-19 16:34:57 -0300

    feat(experience): adding primeng timeline in experience

--- src/app/features/experience/views/experience/experience.component.ts
+++ src/app/features/experience/views/experience/experience.component.ts
@@ -1,23 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, Signal} from '@angular/core';
 import {Experience} from "../../interfaces/experience";
 import {SectionHeadingComponent} from "@shared/components/section-heading/section-heading.component";
 import {Observable} from "rxjs";
 import {TranslocoService} from "@jsverse/transloco";
-import {AsyncPipe} from "@angular/common";
+import {TimelineModule} from "primeng/timeline";
+import {toSignal} from "@angular/core/rxjs-interop";
+import {CardModule} from "primeng/card";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {faBriefcase} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-experience',
   standalone: true,
   imports: [
     SectionHeadingComponent,
-    AsyncPipe
+    TimelineModule,
+    CardModule,
+    FaIconComponent
   ],
   templateUrl: './experience.component.html'
 })
 export class ExperienceComponent {
-  public experienceList$: Observable<Experience[]>;
+  public experienceList$: Signal<Experience[] | undefined>;
+  public workIcon = faBriefcase;
 
   constructor(private translocoService: TranslocoService) {
-    this.experienceList$ = this.translocoService.selectTranslateObject('experienceSection.list') as Observable<Experience[]>;
+    this.experienceList$ = toSignal(this.translocoService.selectTranslateObject('experienceSection.list') as Observable<Experience[]>);
+  }
+
+  public getAlignByLayout(): string {
+    return window.innerWidth >= 1024 ? 'alternate' : 'left';
   }
 }
